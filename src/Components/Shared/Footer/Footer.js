7e999fdd64706da2ad49ec358ed44f58eeccd33b
MarianaMSA <maiasalvador@gmail.com>
2021-10-19 20:21:57 -0300

    primeiro commit

---
+++ src/Components/Shared/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer className="w-100">
+            <div className="container">
+                <p className="text-center"> Desenvolvido por Mariana Salvador.</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
